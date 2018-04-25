720a47008e57ec7afb1a0cfd45fcd80bcbcbc73b
bv <bellavanillo@gmail.com>
2018-04-25 14:22:17 -0700

    pass spec for calculating the age of someone into seconds

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -4,6 +4,6 @@ import { Birthdate } from './../src/galactic.js';
 describe('Birthdate', function(){
   it('should calculate the age of someone into seconds', function(){
     let birthdate = new Birthdate();
-    expect(birthdate.secondsAge(15)).toEqual(473040001);
+    expect(birthdate.secondsAge(15)).toEqual(473040000);
   });
 });
